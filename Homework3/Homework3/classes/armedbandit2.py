51ccfe408a99de5d904dd738223311a347c3ea56
AlbertoHP17 <albdvm@gmail.com>
2015-12-11 00:12:56 -0800

    epsilon greedy working

--- Homework3/Homework3/classes/armedbandit2.py
+++ Homework3/Homework3/classes/armedbandit2.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 import numpy as np
 from scipy import stats
 import random
 import math
-
-import matplotlib.pyplot as plt
-
-#matplotlib inline
 
 class Arm(object):
     pullCount = 0
@@ -16,41 +12,47 @@ class Arm(object):
     current_value = 0.0
 
     probability = 0.0
+    iterations = 10
 
     def __init__(self, mu=1, sigma=5, av=0.0):
         pullCount = 0
         self.mu = mu        #mean
         self.sigma = sigma  # variance
-        self.current_value = 110#av
+        self.current_value = 3
         self.probability = np.random.normal(self.mu, self.sigma)
-
-    def pullArm(self):
+        
+            
+    def pullArm(self, iterations=10):
         self.pullCount += 1
         n = self.pullCount
-
+ 
+        self.iterations = iterations
+ 
         value = self.current_value
-
         self.current_value = ((n -1)/float(n)) * value + (1 / float(n)) * self.getReward()
-
+        
         return self.current_value
 
 
     def getReward(self): #fix this
-        #reward = 0
-        #for i in range(10):
-        #    r = np.random.normal(self.mu, self.sigma)
+        reward = 0
+        #for i in range(self.iterations):
+        #    r = +np.random.normal(self.mu, self.sigma)
         #    if r < self.probability:
         #        reward += 1
 
-        #return reward
-        return np.random.normal(self.mu, math.sqrt(self.sigma))
+        for i in range(self.iterations):
+            reward = reward + np.random.normal(self.mu, self.sigma)
+            
+
+        return reward/self.iterations
+        #return np.random.normal(self.mu, math.sqrt(self.sigma))
 
 
 
 
 
 class ArmedBandit2(object):
-
     n = 10
     _mu = [1, 1.5, 2, 2, 1.75]
     _sigma = [5, 1, 1, 2, 10]
@@ -71,15 +73,11 @@ class ArmedBandit2(object):
                 obj = Arm(self._mu[count], self._sigma[count], np.mean(self._mu))
                 self.arms.append(obj)
 
-            #self.av = np.array([n-1, np.mean(self._mu)]).reshape(1, 2)
-            self.av =  np.array([0, 100])
-            self.av =  np.concatenate((self.av, [[1, 100]]), axis=0)
-            self.av =  np.concatenate((self.av, [[2, 100]]), axis=0)
-            self.av =  np.concatenate((self.av, [[3, 100]]), axis=0)
-            self.av =  np.concatenate((self.av, [[4, 100]]), axis=0)
+ 
             pass
         except Exception as ex:
             print(ex.args)
+
 
     def getArm(self, index):
         return self.arms[index]
@@ -95,21 +93,23 @@ class ArmedBandit2(object):
                 if bestMean < avg:
                     bestMean = avg
                     bestArm = int(u[0])
-            print('Best Mean :' + str(bestMean))
+            
             return bestArm
         except Exception as ex:
             print(ex.args)
 
 
-    def performOneArmRobberyEGreedy(self, epochs=500, epsilon=0.1):
-        plt.xlabel("Plays")
-        plt.ylabel("Avg Reward")
+    def performOneArmRobberyEGreedy(self, epochs=500, iterations=10, epsilon=0.1):
+        retval = []
 
         self.epsilon = epsilon
+        try:           #self.av = np.array([n-1, np.mean(self._mu)])
+            self.av =  np.array([0, np.mean(self._mu)]).reshape(1, 2)
+            self.av =  np.concatenate((self.av, [[1, np.mean(self._mu)]]), axis=0)
+            self.av =  np.concatenate((self.av, [[2, np.mean(self._mu)]]), axis=0)
+            self.av =  np.concatenate((self.av, [[3, np.mean(self._mu)]]), axis=0)
+            self.av =  np.concatenate((self.av, [[4, np.mean(self._mu)]]), axis=0)
 
-        print('starting e-Greedy run ')
-
-        try:
             for i in range(epochs):
                 if random.random() > self.epsilon: #greedy arm selection
                     choice = self.bestArm(self.av)
@@ -117,8 +117,7 @@ class ArmedBandit2(object):
                 else: #random arm selection
                     choice = random.randint(0, len(self.arms)-1)
 
-                print('Selected Arm = ' + str(choice))
-                thisAV = self.arms[choice].pullArm()
+                thisAV = self.arms[choice].pullArm(iterations)
                 self.av = np.concatenate((self.av, [[choice, thisAV]]), axis=0)
 
                 #calculate the percentage the correct arm is chosen (you can plot this instead of reward)
@@ -127,7 +126,10 @@ class ArmedBandit2(object):
                 #calculate the mean reward
                 runningMean = np.mean(self.av[:,1])
 
-                plt.scatter(i, runningMean)
+                retval.append(runningMean)
+
+            return retval
+                
         except Exception as ex:
             print(ex.args)
 
--- Homework3/Homework3/main.py
+++ Homework3/Homework3/main.py
@@ -1,22 +1,97 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
-
-#from classes.armedbandit import ArmedBandit
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from scipy import stats
 from classes.armedbandit2 import ArmedBandit2, Arm
+
 
 if __name__ == '__main__':
 
-#    bandit = ArmedBandit(10)
-#    bandit.performOneArmRobberyEGreedy(500, 0.1)
-#    bandit.performOneArmRobberyEGreedy(500, 0)
-
+    runs = 10
 
     bandit = ArmedBandit2(5)
-    bandit.performOneArmRobberyEGreedy(2500, 0)
+    accumulator = []
+    
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True)
+    x = np.arange(250)
+    
+    ax = axs[0,0]   
+   
+    accumulator = []
+    errors = []
+    y = []
+    for i in range(runs):
+        start = dt.datetime.now()
+        accumulator.append(bandit.performOneArmRobberyEGreedy(epochs=250, iterations=10, epsilon=0))
+        print('Run 10-0 #' + str(i) + ' took ' + str((dt.datetime.now() - start).total_seconds()) + ' seconds') 
 
-    #a = bandit.getArm(3)
+    accumulator2 = np.array(accumulator)
+    for i in range(len(accumulator[0])):
+        errors.append(stats.sem(accumulator2[: ,i], axis=None, ddof=0))
+        y.append(np.mean(accumulator2[: ,i]))
+ 
+    ax.plot(x, y, 'r')
+    ax.errorbar(x, y, yerr=errors) #, fmt='o')
+    
+    #--------------------------------------------
+    accumulator = []
+    errors = []
+    y = []
+    for i in range(runs):
+        start = dt.datetime.now()
+        accumulator.append(bandit.performOneArmRobberyEGreedy(epochs=250, iterations=10, epsilon=0.1))
+        print('Run 10-0.1 #' + str(i) + ' took ' + str((dt.datetime.now() - start).total_seconds()) + 'seconds') 
 
-    #for i in range(0,200):
-    #    print('Arm pull ' + str(a.pullArm()))
+    accumulator2 = np.array(accumulator)
+    for i in range(len(accumulator[0])):
+        errors.append(stats.sem(accumulator2[: ,i], axis=None, ddof=0))
+        y.append(np.mean(accumulator2[: ,i]))
+
+    ax.plot(x, y, 'g')
+    ax.errorbar(x, y, yerr=errors) #, fmt='o')
+    
+    #--------------------------------------------
+    ax = axs[0,1]   
+
+    accumulator = []
+    errors = []
+    y = []
+
+    for i in range(runs):
+        start = dt.datetime.now()
+        accumulator.append(bandit.performOneArmRobberyEGreedy(epochs=250, iterations=100, epsilon=0))
+        print('Run 100-0 #' + str(i) + ' took ' + str((dt.datetime.now() - start).total_seconds()) + 'seconds') 
+
+    accumulator2 = np.array(accumulator)
+    for i in range(len(accumulator[0])):
+        errors.append(stats.sem(accumulator2[: ,i], axis=None, ddof=0))
+        y.append(np.mean(accumulator2[: ,i]))
+ 
+    ax.plot(x, y, 'r')
+    ax.errorbar(x, y, yerr=errors) #, fmt='o')
+    
+ 
+    accumulator = []
+    errors = []
+    y = []
+    for i in range(runs):
+        start = dt.datetime.now()
+        accumulator.append(bandit.performOneArmRobberyEGreedy(epochs=250, iterations=100, epsilon=0.1))
+        print('Run 100-0.1 #' + str(i) + ' took ' + str((dt.datetime.now() - start).total_seconds()) + 'seconds') 
+
+    accumulator2 = np.array(accumulator)
+    for i in range(len(accumulator[0])):
+        errors.append(stats.sem(accumulator2[: ,i], axis=None, ddof=0))
+        y.append(np.mean(accumulator2[: ,i]))
+ 
+    ax.plot(x, y, 'g')
+    ax.errorbar(x, y, yerr=errors) #, fmt='o')
+
+    
+    fig.suptitle('Variable errorbars')
+
+    plt.show()
 
     pass
